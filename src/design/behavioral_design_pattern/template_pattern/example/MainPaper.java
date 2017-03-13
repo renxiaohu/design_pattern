bbe9a1891fd46a9bd74ca09ca5661a9584b50b9f
renxiaohu <517128279@qq.com>
2017-03-13 20:27:08 +0800

    template

--- src/design/behavioral_design_pattern/template_pattern/example/MainPaper.java
+++ src/design/behavioral_design_pattern/template_pattern/example/MainPaper.java
@@ -4,19 +4,19 @@ package design.behavioral_design_pattern.template_pattern.example;
  * Created by Administrator on 2016/12/13.
  */
 public class MainPaper {
-    public static void main(String arges[]){
+    public static void main(String arges[]) {
         System.out.print("学生甲抄的试卷：");
         TestPaper studentA = new TestPaperA();
-        studentA.Answer1();
-        studentA.Answer2();
-        studentA.Answer3();
+        studentA.TestQuestion1();
+        studentA.TestQuestion2();
+        studentA.TestQuestion3();
         System.out.print("\n");
 
 
         System.out.print("学生乙抄的试卷：");
         TestPaper studentB = new TestPaperB();
-        studentA.Answer1();
-        studentA.Answer2();
-        studentA.Answer3();
+        studentA.TestQuestion1();
+        studentA.TestQuestion2();
+        studentA.TestQuestion3();
     }
 }
--- src/design/behavioral_design_pattern/template_pattern/example/TestPaper.java
+++ src/design/behavioral_design_pattern/template_pattern/example/TestPaper.java
@@ -6,37 +6,37 @@ package design.behavioral_design_pattern.template_pattern.example;
  */
 public abstract class TestPaper {
     public void TestQuestion1(){
-        System.out.print("杨过得到，后来给了郭靖，练成倚天剑,屠龙刀的玄铁可能是【" +
+        System.out.print("\n杨过得到，后来给了郭靖，练成倚天剑,屠龙刀的玄铁可能是【" +
                 "" +"a：球磨铸铁"+
                 "" +"b：马口铁"+
                 "" +"c：高速合金钢"+
                 "" +"d：碳素纤维"+
-                "】");
+                "】\n");
         System.out.print("答案："+Answer1());
     }
     protected String Answer1(){
         return "";
     }
     public void TestQuestion2(){
-        System.out.print("杨过，陈英，陆无双铲除了情花，造成【" +
+        System.out.print("\n杨过，陈英，陆无双铲除了情花，造成【" +
                 "" +"a：使这种植物不再害人"+
                 "" +"b：是一种珍惜物种灭绝"+
                 "" +"c：破坏了那个生态圈的生态平衡"+
                 "" +"d：造成该地区沙漠化"+
-                "】");
+                "】\n");
         System.out.print("答案："+Answer2());
     }
     protected String Answer2(){
         return "";
     }
     public void TestQuestion3(){
-        System.out.print("蓝凤凰致使华山师徒呕吐不止，如果你是大夫，会开神马药给他们,【" +
+        System.out.print("\n蓝凤凰致使华山师徒呕吐不止，如果你是大夫，会开神马药给他们,【" +
                 "" +"a：阿司匹林"+
                 "" +"b：牛黄解毒片"+
                 "" +"c：氟哌酸"+
                 "" +"d：喝大量生牛奶"+
                 "" +"e：以上都不对"+
-                "】");
+                "】\n");
         System.out.print("答案："+Answer3());
     }
     protected String Answer3(){
